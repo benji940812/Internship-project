8c6c07258f56a077a3b0d1eee9781142ef69e0e0
Hyunchang Jin <b@Hyunchangs-MacBook-Pro.local>
2025-10-27 14:19:27 -0700

    headless Chrome & Firefox set up and updated tests

--- features/environment.py
+++ features/environment.py
@@ -1,34 +1,56 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 from app.application import Application
+import os
+
 
 def browser_init(context):
-    """
-    :param context: Behave context
-    """
-    driver_path = ChromeDriverManager().install()
-    service = Service(driver_path)
-    context.driver = webdriver.Chrome(service=service)
+    # Default browser is Chrome.
+    browser = os.getenv("BROWSER", "chrome")
 
-    context.driver.maximize_window()
+    if browser == "firefox":
+        options = FirefoxOptions()
+        options.add_argument("--headless")
+        options.add_argument("--width=1920")
+        options.add_argument("--height=1080")
+
+        service = FirefoxService(GeckoDriverManager().install())
+        context.driver = webdriver.Firefox(service=service, options=options)
+        print("Running tests in Firefox (headless mode)")
+    else:
+        options = ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
+
+        service = Service(ChromeDriverManager().install())
+        context.driver = webdriver.Chrome(service=service, options=options)
+        print("Running tests in Chrome (headless mode)")
+
     context.driver.implicitly_wait(4)
-
     context.app = Application(context.driver)
 
+
 def before_scenario(context, scenario):
-    print('\nStarted scenario: ', scenario.name)
+    print("\nStarted scenario:", scenario.name)
     browser_init(context)
 
 
 def before_step(context, step):
-    print('\nStarted step: ', step)
+    print("Started step:", step)
 
 
 def after_step(context, step):
-    if step.status == 'failed':
-        print('\nStep failed: ', step)
+    if step.status == "failed":
+        print("Step failed:", step)
 
 
-def after_scenario(context, feature):
+def after_scenario(context, scenario):
     context.driver.quit()
